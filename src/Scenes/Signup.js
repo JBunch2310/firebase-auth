2ff73a6796a4358a597ec541a0ca655166797702
Jamie Bunch <JamieBunch@ymail.com>
2022-02-16 16:48:58 -0500

    update log out button

--- src/Scenes/Signup.js
+++ src/Scenes/Signup.js
@@ -1,69 +1,56 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
-import { app } from "../ConnectAuth";
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { app } from '../ConnectAuth'
 
 export default function Signup({ setUser }) {
-  const [email, setEmail] = useState(" ");
-  const [password, setPassword] = useState(" ");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const provider = new GoogleAuthProvider();
   const auth = getAuth(app);
   const handleFormSubmit = (event) => {
     event.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        //setUser
-        setUser(result.user);
-        //navigate to home
-        navigate("/");
+      .then(result => {
+        setUser(result.user)
+        navigate('/')
       })
-      .catch(alert);
-  };
+      .catch(alert)
+  }
   const handleGoogleLogin = () => {
     signInWithPopup(auth, provider)
-      .then((result) => {
-        //setUser
-        setUser(result.user);
-        //navigate to home
-        navigate("/");
+      .then(result => {
+        setUser(result.user)
+        navigate('/')
       })
-      .catch(alert);
-  };
+      .catch(alert)
+  }
   return (
     <>
-      <h2>Signup</h2>
-      <br />
+      <h1>Signup</h1>
+      <hr />
       <form onSubmit={handleFormSubmit}>
         <label>Email:
-          <input
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}/>
+          <input type="email" value={email}
+            onChange={e => setEmail(e.target.value)} />
         </label>
+        <br />
         <label>Password:
-          <input
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}/>
+          <input type="password" value={password}
+            onChange={e => setPassword(e.target.value)} />
         </label>
+        <br />
         <input type="submit" value="Sign up" />
       </form>
       <button
         onClick={handleGoogleLogin}
         style={{
-          backgroundColor: "black", 
-          color: "white", 
-          border: "none" }}>
+          backgroundColor: 'black',
+          color: 'white',
+          border: 'none' }}>
         Sign in with Google</button>
-      <p>
-        Already a user? <Link to="login">Login</Link>
-      </p>
+      <p>Already a user? <Link to="/login">Login</Link></p>
     </>
   )
 }
--- src/Scenes/Welcome.js
+++ src/Scenes/Welcome.js
@@ -1,14 +1,29 @@
-export default function Welcome({user}) {
-    console.log(user) // email, displayName, photoURL
-    return (
+import { useNavigate } from 'react-router-dom'
+import { getAuth, signOut } from 'firebase/auth'
+
+export default function Welcome({ user }) {
+  console.log(user) // email, displayName, photoURL
+
+  const auth = getAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        localStorage.clear()
+        navigate('/login')
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  return (
     <>
-    <h1>Welcome</h1>
-    <h2>{user.email}</h2>
-    {user.photoURL && 
-    <img src={user.photoURL}
-    alt="Profile picture of logged-in user" />}
+      <h1>Welcome</h1>
+      <h2>{user.displayName || user.email}</h2>
+      {user.photoURL && <img src={user.photoURL} alt='Profile picture of logged-in user' width='20%' />}
+      <button onClick={handleLogout}>Logout</button>
     </>
-    )
+  )
 }
-
-
