35dd2df513519aa17aaeaa81a23bfef2d457b61c
Jamie Bunch <JamieBunch@ymail.com>
2022-02-16 14:05:52 -0500

    with google add in

--- src/Scenes/Signup.js
+++ src/Scenes/Signup.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { app } from "../ConnectAuth";
 
 export default function Signup({ setUser }) {
   const [email, setEmail] = useState(" ");
   const [password, setPassword] = useState(" ");
   const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    const auth = getAuth(app);
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         //setUser
@@ -19,35 +25,45 @@ export default function Signup({ setUser }) {
       })
       .catch(alert);
   };
-
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        //setUser
+        setUser(result.user);
+        //navigate to home
+        navigate("/");
+      })
+      .catch(alert);
+  };
   return (
     <>
       <h2>Signup</h2>
       <br />
       <form onSubmit={handleFormSubmit}>
-        <label>
-          {" "}
-          Email:
+        <label>Email:
           <input
             type="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+            onChange={e => setEmail(e.target.value)}/>
         </label>
-        <label>
-          {" "}
-          Password:
+        <label>Password:
           <input
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+            onChange={e => setPassword(e.target.value)}/>
         </label>
         <input type="submit" value="Sign up" />
       </form>
+      <button
+        onClick={handleGoogleLogin}
+        style={{
+          backgroundColor: "black", 
+          color: "white", 
+          border: "none" }}>
+        Sign in with Google</button>
       <p>
         Already a user? <Link to="login">Login</Link>
       </p>
     </>
-  );
+  )
 }
--- src/Scenes/Welcome.js
+++ src/Scenes/Welcome.js
@@ -1,5 +1,14 @@
-export default function Welcome() {
-    return <h1>Welcome</h1>
+export default function Welcome({user}) {
+    console.log(user) // email, displayName, photoURL
+    return (
+    <>
+    <h1>Welcome</h1>
+    <h2>{user.email}</h2>
+    {user.photoURL && 
+    <img src={user.photoURL}
+    alt="Profile picture of logged-in user" />}
+    </>
+    )
 }
 
 
